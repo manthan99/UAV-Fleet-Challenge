31777adcb030416d41fdd07f65d2c595f26a7747
manthan99 <manthanhemangpatel@gmail.com>
2019-12-06 00:24:31 +0000

    modify script and launch

--- drone1.sh
+++ drone1.sh
@@ -1,20 +1,19 @@
 #sudo killall roscore &
 #sleep 2 &
 
-#sudo date 120900002019.00
+sudo date 120900002019.00
 sleep 1
 sudo route add -net 224.0.0.0 netmask 224.0.0.0 wlan0 
 sleep 1 
-roscore &
+roslaunch swarm_search apm1.launch &
 sleep 4 
 rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.1 &
 sleep 2 
 rosrun master_sync_fkie master_sync &
-sleep 1 
-roslaunch swarm_search apm1.launch &
 sleep 1
 roslaunch swarm_search master1.launch &
-
+sleep 1
+python ~/ardupilot_ws/src/UAV_Fleet_Challenge/Planning/waypoint_generator/src/path1.py &
 
 
 
