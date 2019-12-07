e0f9a69639a421aebf5b899211ecd64f5c1878ad
Parakh Agarwal <parakh08@gmail.com>
2019-12-08 02:43:00 +0530

    updated for new mavros messages

--- Planning/waypoint_generator/src/path.py
+++ Planning/waypoint_generator/src/path.py
@@ -38,11 +38,11 @@ def flag_callback(data):
 
 def listener():
     global pub1
-    rospy.init_node('path_drone1', anonymous=True)
+    rospy.init_node('path_drone2', anonymous=True)
     pos_sub = rospy.Subscriber('/chatter', point_list, posCallback)
-    current_pos = rospy.Subscriber("/drone1/mavros/global_position/global",NavSatFix, current_callback)
-    flags = rospy.Subscriber("/drone1/flags",Twist,flag_callback)
-    pub1 = rospy.Publisher('/drone1/ROI_flow', point_list, queue_size=5)
+    current_pos = rospy.Subscriber("/drone2/mavros/global_position/global",NavSatFix, current_callback)
+    flags = rospy.Subscriber("/drone2/flags",Twist,flag_callback)
+    pub1 = rospy.Publisher('/drone2/ROI_flow', point_list, queue_size=5)
     rospy.spin()
 
 
