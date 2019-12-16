b3861076dd2fceaaabdd7f915c451a31796eca1c
manthan99 <manthanhemangpatel@gmail.com>
2019-12-16 19:21:54 +0530

    add Gmap SIP and 6 SIP scripts

--- gnd_station.sh
+++ gnd_station.sh
@@ -1,8 +1,10 @@
 roscore &
+sleep 2 
+sudo route add -net 224.0.0.0 netmask 224.0.0.0 wlo1
 sleep 3
 rosrun master_sync_fkie master_sync &
 sleep 3 
-rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.1 
+rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.1 &
 
 
 
