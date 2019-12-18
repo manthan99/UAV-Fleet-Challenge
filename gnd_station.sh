f92a88ef6b77564b29744ab73e106e01c00fa68a
shreyshrivastava4799 <shrivastava.shrey@ymail.com>
2019-12-18 15:04:10 +0530

    modify gnd station script

--- gnd_station.sh
+++ gnd_station.sh
@@ -1,10 +1,15 @@
 roscore &
+sleep 2
+sudo ifconfig wlo1 down
+sleep 1
+sudo ifconfig wlo1 up
 sleep 2 
 sudo route add -net 224.0.0.0 netmask 224.0.0.0 wlo1
 sleep 3
-rosrun master_sync_fkie master_sync &
-sleep 3 
 rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.1 &
+sleep 3
+rosrun master_sync_fkie master_sync &
+
 
 
 
